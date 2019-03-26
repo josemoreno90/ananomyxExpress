b27cd5fbb25f3ede33e92a14ffd9ebbb883cf07a
Jose Moreno <morenojr90@yahoo.com>
2019-03-26 11:32:15 -0700

    fix port and add gitignore

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
+
+app.get('/' , (req, res) => res.send('Hello World'))
+
+app.listen(port, () => console.log(`App Listening on Port: ${port}`))
